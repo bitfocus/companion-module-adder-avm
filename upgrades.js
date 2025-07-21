66228debff324e9c8348fa1bd9196ea734907870
Zach Larsen <zach_larsen@adder.com>
2025-07-21 17:26:29 +0100

    Add upgrade file to prevent breaking feedbacks. This will add a default value for the feedback dropdown

--- upgrades.js
+++ upgrades.js
@@ -1,13 +1,28 @@
 module.exports = [
-	/*
-	 * Place your upgrade scripts here
-	 * Remember that once it has been added it cannot be removed!
-	 */
-	// function (context, props) {
-	// 	return {
-	// 		updatedConfig: null,
-	// 		updatedActions: [],
-	// 		updatedFeedbacks: [],
-	// 	}
-	// },
+	function (context, props) {
+		const feedbackTypesToFix = ['presetStatusBool', 'presetStatus', 'videoStatusBool'];
+		let updatedFeedbacks = [];
+		const presets = JSON.parse(context.currentConfig.presets || '[{ id: "None", label: "None" }]');
+		for (const feedback of props.feedbacks){
+			if (feedbackTypesToFix.includes(feedback.feedbackId)) {
+				if (!feedback.options.preset) {
+					try {
+						
+						if (presets.length > 0) {
+							feedback.options.preset = presets[0].id;
+							updatedFeedbacks.push(feedback);
+							console.log("Updated")
+					}
+					}catch (e){
+					console.log(e)	
+					}
+				}
+			}
+		}
+		return {
+			updatedConfig: null,
+			updatedActions: [],
+			updatedFeedbacks: updatedFeedbacks
+		}
+	}
 ]
